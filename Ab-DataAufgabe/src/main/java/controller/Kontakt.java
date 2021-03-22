f8ede4b91c8a9785640f484ace1f003e4de94a82
abdoabbas22 <69873372+abdoabbas22@users.noreply.github.com>
2021-03-22 15:08:54 +0100

    Update Kontakt.java
    ID Updated

--- Ab-DataAufgabe/src/main/java/controller/Kontakt.java
+++ Ab-DataAufgabe/src/main/java/controller/Kontakt.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-import org.springframework.data.annotation.Id;
+import javax.persistence.Id;
 
 @Entity
 public class Kontakt {
